1bb11b1199d2c87b75bbc0382f69956e2ef78b6a
Christine <z5157602@ad.unsw.edu.au>
2022-02-15 12:57:40 +1100

    plot a heatmap of the h5 data, with 5 genes of interest and 41 cell types

--- WebPortal/main.py
+++ WebPortal/main.py
@@ -1,7 +1,10 @@
+from base64 import decode
 from flask import Flask, send_from_directory
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import pandas as pd
+import h5py
+import numpy as np
 import json
 
 app = Flask(__name__, static_url_path='/static')
@@ -24,40 +27,35 @@ def helloworld():
         response = f.read()
     return response
 
-class GeneExpression(Resource):
+# use Carsten's h5 data
+class H5GeneExpression(Resource):
     def get(self):
-        df = pd.read_excel("test_data_1.xlsx")
-        df = df.round(decimals=3)
-        print(df)
-        data_string = df.to_json()  # string type
-        return json.loads(data_string)  # json object
+        h5_data = h5py.File('./static/scData/condensed_lung_atlas.h5',"r")
+        df = pd.DataFrame(data=np.array(h5_data['cell_type']\
+            ['gene_expression_average']['block0_values']),\
+            index=np.array(h5_data['cell_type']['gene_expression_average']['axis1'])\
+            ,columns=np.array(h5_data['cell_type']['gene_expression_average']['axis0'])).T
+        
+        # current index in the dataframe is writtern as binary string.
+        # We need to convert it into normal string
+        new_index=[]
+        for i in df.index:
+            new_index.append(i.decode('utf-8'))
+        # Similarly for columns name
+        new_columns=[]
+        for i in df.columns:
+            new_columns.append(i.decode('utf-8'))
 
-# New data set for gene expression across cell type
-class GeneExpression_2(Resource):
-    def get(self):
-        df = pd.read_csv("pbmc3k_subset_data.csv")
-        # this data is used for heatmap and scatter plot
-        # for each gene, we want the average gene expression level in each cell type
-        df_avg_exp = df.groupby(['Population']).mean()
-        data_string = df_avg_exp.to_json()
-        return json.loads(data_string)
+        df.index = new_index
+        df.columns = new_columns
 
-class GeneExpression_3(Resource):
-    def get(self):
-        # if we want a vilon plot for all gene expression in B cells
-        df = pd.read_csv("pbmc3k_subset_data.csv")
-        df_bCells = df[df['Population'] == 'B cells'].drop(['CellBarcode'],axis=1)
-        df_bCells.set_index('Population',inplace=True)
-        data_dic = {}
-        for gene in df_bCells.columns:
-            exp_data = list(df_bCells[gene])
-            data_dic[gene] = exp_data
-        return data_dic
+        # Select 5 genes of interest:
+        plot_df = df.filter(items = ['Car4','Vwf', 'Col1a1', 'Ptprc', 'Ms4a1'], axis=0)
+        plot_data = plot_df.to_json()
+        return json.loads(plot_data)
 
 # this is an API endpoint (return data)
-api.add_resource(GeneExpression, '/data')
-api.add_resource(GeneExpression_2, '/data2')
-api.add_resource(GeneExpression_3, '/data3')
+api.add_resource(H5GeneExpression, '/data')
 
 if __name__ == '__main__':
     app.run(debug=True)

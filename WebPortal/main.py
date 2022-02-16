fbd48fb9868fe517a3594d8aae9b693b538817eb
Christine <z5157602@ad.unsw.edu.au>
2022-02-17 00:55:00 +1100

    adding scatter plot when number of gene searched is 2

--- WebPortal/main.py
+++ WebPortal/main.py
@@ -64,8 +64,47 @@ class H5GeneExpression(Resource):
 
         return json.loads(plot_data)
 
+class plotsForSeachGenes(Resource):
+    def get(self):
+        h5_data = h5py.File('./static/scData/condensed_lung_atlas.h5',"r")
+        df = pd.DataFrame(data=np.array(h5_data['cell_type']\
+            ['gene_expression_average']['block0_values']),\
+            index=np.array(h5_data['cell_type']['gene_expression_average']['axis1'])\
+            ,columns=np.array(h5_data['cell_type']['gene_expression_average']['axis0'])).T
+        
+        new_index=[]
+        for i in df.index:
+            new_index.append(i.decode('utf-8'))
+        # Similarly for columns name
+        new_columns=[]
+        for i in df.columns:
+            new_columns.append(i.decode('utf-8'))
+
+        df.index = new_index
+        df.columns = new_columns
+
+        gene_names = request.args.get('gene_names')
+        a_gene_names = gene_names.split(",")
+        if len(a_gene_names) == 2:
+            result = {}
+            plot_df = df.filter(items = a_gene_names, axis=0)
+            gene1 = plot_df.index[0]
+            gene2 = plot_df.index[1]
+
+            gene1_expr = list(plot_df.loc[gene1])
+            gene2_expr = list(plot_df.loc[gene2])
+            # plot_data = plot_df.to_json()
+            result["gene1_name"]= gene1
+            result["gene2_name"] = gene2
+            result["gene1_expr"] = gene1_expr
+            result["gene2_expr"] = gene2_expr
+            result["cell_types"] = list(plot_df.columns)
+
+        return result
+
 # this is an API endpoint (return data)
 api.add_resource(H5GeneExpression, '/data')
+api.add_resource(plotsForSeachGenes, '/2_genes')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- WebPortal/static/js/searchGenesPlot.js
+++ WebPortal/static/js/searchGenesPlot.js
@@ -2,53 +2,107 @@
 $( "#searchOnClick" ).click(function() {
   // action here when clicking the search button
   var gene_name = $('#searchGeneName').val();
-  
-  // sent gene names to the API
-  $.ajax({
-    type:'GET',
-    url:'http://127.0.0.1:5000/data',
-    data: "gene_names=" + gene_name,
-    success: function(result){
-      console.log(result)
-      // x-axis: 5 genes of interest
-      let x_axis = Object.keys(result[Object.keys(result)[0]]);
-      // y-axis:41 cell types
-      let y_axis = Object.keys(result);
-      let data_content = [];
-      for (var i = 0; i < Object.keys(result).length; i++) {
-          cell_type = Object.keys(result)[i] // get the cell_type name as a string
-          all_gene_expression = result[cell_type]         // find it from the dictionary as a key
-          data_content.push(Object.values(all_gene_expression))
-      }
-      var data = [
-          {
-              z: data_content,
-              x: x_axis,
-              y: y_axis,
-              type: 'heatmap',
-              hoverongaps: false
-          }
-        ];
-      var layout = {
-          title: 'Heatmap of gene expression level in selected cell types',
-          xaixs: {
-              title: {
-                  text: 'Genes of interest',
-                  standoff: 20
-              }},
-          yaxis: {
-              title: {
-                  text: 'Cell types',
-                  standoff: 40
-              }},
-      };
+  const gene_array = gene_name.split(",")
+  if (gene_array.length == 2) {
+    $.ajax({
+      type:'GET',
+      url:'http://127.0.0.1:5000/2_genes',
+      data: "gene_names=" + gene_name,
+      success: function(result){
+        // compare the expression level of 2 specific genes across all cell types
+        let expr1 = result["gene1_expr"];
+        let expr2 = result["gene2_expr"];
+        let cell_types =result["cell_types"];
+        var trace1 = {
+            x: expr1,
+            y: expr2,
+            mode: 'markers+text',
+            type: 'scatter',
+            text: cell_types,
+            textposition: 'bottom center',
+            textfont: {
+              family:  'Raleway, sans-serif'
+            },
+            marker: { size: 12 }
+        };
+            
+        var data = [ trace1 ];
         
-        Plotly.newPlot(document.getElementById('h5_data_plot'), data,layout);  
-    },
-    error: function (e) {
-      alert('Request data Failed')
-    }
-  });
-
-
+        var layout = {
+            xaxis: {
+                title: {
+                    text: result['gene1_name']+' &nbsp expression',
+                    font: {
+                        family: 'Courier New, monospace',
+                        size: 18,
+                        color: '#7f7f7f'
+                    }
+                },
+                range: [ 0, 3 ]
+            },
+            yaxis: {
+                title: {
+                    text: result['gene1_name']+'&nbsp expression',
+                    font: {
+                        family: 'Courier New, monospace',
+                        size: 18,
+                        color: '#7f7f7f'
+                    }
+                },
+                range: [0, 6]
+            },
+            title:'Average expression level of 2 selected gene in different cell types'
+        };
+        
+        Plotly.newPlot(document.getElementById('scatter_plot'), data, layout);
+      }
+    });
+  } else {
+    // sent gene names to the API
+    $.ajax({
+      type:'GET',
+      url:'http://127.0.0.1:5000/data',
+      data: "gene_names=" + gene_name,
+      success: function(result){
+        console.log(result)
+        // x-axis: 5 genes of interest
+        let x_axis = Object.keys(result[Object.keys(result)[0]]);
+        // y-axis:41 cell types
+        let y_axis = Object.keys(result);
+        let data_content = [];
+        for (var i = 0; i < Object.keys(result).length; i++) {
+            cell_type = Object.keys(result)[i] // get the cell_type name as a string
+            all_gene_expression = result[cell_type]         // find it from the dictionary as a key
+            data_content.push(Object.values(all_gene_expression))
+        }
+        var data = [
+            {
+                z: data_content,
+                x: x_axis,
+                y: y_axis,
+                type: 'heatmap',
+                hoverongaps: false
+            }
+          ];
+        var layout = {
+            title: 'Heatmap of gene expression level in selected cell types',
+            xaixs: {
+                title: {
+                    text: 'Genes of interest',
+                    standoff: 20
+                }},
+            yaxis: {
+                title: {
+                    text: 'Cell types',
+                    standoff: 40
+                }},
+        };
+          
+          Plotly.newPlot(document.getElementById('h5_data_plot'), data,layout);  
+      },
+      error: function (e) {
+        alert('Request data Failed')
+      }
+    });
+  }
 });

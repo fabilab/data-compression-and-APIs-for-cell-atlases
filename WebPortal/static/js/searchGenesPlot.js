6f22034a44ab200b20233618fd2badab6f27b6b4
Christine <yingxu0928@gmail.com>
2022-02-18 13:01:35 +1100

    quick update

--- WebPortal/static/js/searchGenesPlot.js
+++ WebPortal/static/js/searchGenesPlot.js
@@ -12,43 +12,9 @@ $( "#searchOnClick" ).click(function() {
       type:'GET',
       url:'http://127.0.0.1:5000/2_genes',
       data: "gene_names=" + gene_name,
-      success: function(result){
-        // compare the expression level of 2 specific genes across all cell types
-        let expr1 = result["gene1_expr"];
-        let expr2 = result["gene2_expr"];
-        let cell_types =result["cell_types"];
-        var trace1 = {
-            x: expr1,
-            y: expr2,
-            mode: 'markers+text',
-            type: 'scatter',
-            text: cell_types,
-            textposition: 'bottom center',
-            textfont: {
-              family:  'Raleway, sans-serif'
-            },
-            marker: { size: 12 }
-        };
-            
-        var data = [ trace1 ];
-        
-        var layout = {
-            xaxis: {
-                title: {
-                  text: result['gene1_name']+' expression',
-                },
-                range: [ 0, 5 ]
-            },
-            yaxis: {
-                title: {
-                  text: result['gene2_name']+' expression',
-              },
-              range: [0, 5]
-            },
-            title:'Correlation between ' + result['gene1_name'] + ' and ' + result['gene2_name']
-        };
-        
-        Plotly.newPlot(document.getElementById('scatter_plot'), data, layout);
+      success: ScatterPlot,
+      error: function (e) {
+        alert('Request data Failed')
       }
     });
   }

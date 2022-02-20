6c65bb6a9248912d9cdac48645a4b048b6e03fba
Christine <yingxu0928@gmail.com>
2022-02-20 19:49:01 +1100

    adding a reset button to reset the heatmap, remove any input in the search bar

---
+++ WebPortal/static/js/resetPage.js
@@ -0,0 +1,19 @@
+$( "#resetOnClick" ).click(function() {
+    if(! $('#scatter_plot').is('empty')) {
+        $('#scatter_plot').empty();
+      }
+    $.ajax({
+        type:'GET',
+        url:'http://127.0.0.1:5000/dataOrigin',
+        data: "gene_names=Car4,Vwf,Col1a1,Ptprc,Ms4a1",
+        dataType:'json',
+        success: HeatMap,
+        error: function (e) {
+        alert('Request data Failed')
+        }
+    });
+    $("#searchGeneName").val("");
+    $("#originalTab").addClass('is-active');
+    $("#logTab").removeClass('is-active');
+    $("#hierachicalTab").removeClass('is-active');
+})

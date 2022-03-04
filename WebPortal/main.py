1bcd6612b9070c9435b01633af80ede326719a3c
Christine <yingxu0928@gmail.com>
2022-03-04 20:32:05 +1100

    clean up code

--- WebPortal/main.py
+++ WebPortal/main.py
@@ -35,8 +35,6 @@ class geneExp(Resource):
         gene_names = request.args.get('gene_names')
         plot_type = request.args.get('plot_type')
         data_type = request.args.get('data_type')
-        # print(plot_type)
-        # print(data_type)
         
         df = data_preprocessing(gene_names)
         if df is None:
@@ -52,61 +50,6 @@ class geneExp(Resource):
             df = df.iloc[new_order]
         
         return json.loads(df.to_json())
-
-# use Carsten's h5 data
-class geneExpOriginal(Resource):
-    def get(self):
-        # # Select 5 genes of interest:
-        # plot_df = df.filter(items = ['Car4','Vwf', 'Col1a1', 'Ptprc', 'Ms4a1'], axis=0)
-
-        # get the name of genes input by the web user
-        gene_names = request.args.get('gene_names')
-        df = data_preprocessing(gene_names)
-        if df is None:
-            return None
-        return json.loads(df.to_json())
-
-class geneExpLog(Resource):
-    def get(self):
-        gene_names = request.args.get('gene_names')
-        df = data_preprocessing(gene_names)
-        if df is None:
-            return None
-        log_data = np.log10(0.1+df)
-
-        return json.loads(log_data.to_json())
-
-class geneExpHieracical(Resource):
-    def get(self):
-        gene_names = request.args.get('gene_names')
-        df = data_preprocessing(gene_names)
-        if df is None:
-            return None
-        
-        hierachical_data = np.log10(0.1+df)
-        # Hierachical Clustering
-        distance = pdist(hierachical_data.values)
-        Z = linkage(distance,optimal_ordering=True)
-
-        new_order = leaves_list(Z)
-        hierachical_data = hierachical_data.iloc[new_order]
-        return json.loads(hierachical_data.to_json())
-
-class geneExpHieracicalOriginal(Resource):
-    def get(self):
-        gene_names = request.args.get('gene_names')
-        df = data_preprocessing(gene_names)
-        if df is None:
-            return None
-        
-        hierachical_data = df
-        # Hierachical Clustering
-        distance = pdist(hierachical_data.values)
-        Z = linkage(distance,optimal_ordering=True)
-
-        new_order = leaves_list(Z)
-        hierachical_data = hierachical_data.iloc[new_order]
-        return json.loads(hierachical_data.to_json())
 
 class plotsForSeachGenes(Resource):
     def get(self):
@@ -136,10 +79,6 @@ class plotsForSeachGenes(Resource):
 
 # this is an API endpoint (return data)
 api.add_resource(geneExp, '/data')
-api.add_resource(geneExpOriginal, '/dataOrigin')
-api.add_resource(geneExpLog, '/dataLog')
-api.add_resource(geneExpHieracical, '/dataHierachical')
-api.add_resource(geneExpHieracicalOriginal, '/dataHierachicalOriginal')
 api.add_resource(plotsForSeachGenes, '/2_genes')
 
 if __name__ == '__main__':

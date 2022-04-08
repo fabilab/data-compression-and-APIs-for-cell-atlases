6f006cd240d9e8842ba93b0b9c57272b5a280ba4
Fabio Zanini <fabio.zanini@fastmail.fm>
2022-04-08 17:15:13 +1000

    Works from inside docker

--- WebPortal/main.py
+++ WebPortal/main.py
@@ -102,4 +102,5 @@ api.add_resource(plotsForSeachGenes, '/2_genes')
 api.add_resource(geneExpTime, '/data_timepoint')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)

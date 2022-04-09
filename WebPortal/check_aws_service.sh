36a87b700e1714b452dcbeff807e433341332c52
Fabio Zanini <fabio.zanini@fastmail.fm>
2022-04-09 16:52:05 +1000

    New version with the fixes

--- WebPortal/check_aws_service.sh
+++ WebPortal/check_aws_service.sh
@@ -1,8 +1,14 @@
 # Create service
-aws lightsail create-container-service --service-name compressed-atlas --power small --scale 1
+aws lightsail create-container-service --service-name compressed-atlas-service --power small --scale 1
 
 # Check service
-aws lightsail get-container-services --service-name compressed-atlas
+aws lightsail get-container-services --service-name compressed-atlas-service
+
+# Push image to service
+aws lightsail push-container-image --service-name compressed-atlas-service --label compressed-atlas --image compressed-atlas
+
+# Deploy container onto a public URL
+aws lightsail create-container-service-deployment --service-name compressed-atlas-service --containers file://containers.json --public-endpoint file://public-endpoint.json
 
 # Delete service
 #aws lightsail delete-container-service --service-name compressed-atlas
